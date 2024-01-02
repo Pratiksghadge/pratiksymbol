498775860a553d73445ff39d8ffaf07af8169c5c
Pratiksghadge <ghadgepratik317@gmail.com>
2024-01-02 13:52:56 +0530

    code

--- practice.js
+++ practice.js
@@ -22,6 +22,18 @@ links.forEach(link => {
     })
 });
 
+function showSidebar() {
+    var sidebar = document.getElementById('sidebar');
+    sidebar.style.display = (sidebar.style.display === 'none' || sidebar.style.display === '') ? 'block' : 'flex';
+}
+
+document.addEventListener('click', function (event) {
+    var sidebar = document.getElementById('sidebar');
+    var menuIcon = document.querySelector('.ri-menu-3-line');
+    if (!sidebar.contains(event.target) && event.target !== menuIcon) {
+        sidebar.style.display = 'none';
+    }
+});
 
    
 
