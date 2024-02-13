6eb4c29abcec89f34eba3098068c9c646c770fe7
Pratiksghadge <ghadgepratik317@gmail.com>
2024-02-13 14:06:01 +0530

    nav bar change

--- foodHub/food.js
+++ foodHub/food.js
@@ -26,3 +26,43 @@ document.getElementById("img").addEventListener("mouseover", function () {
 document.getElementById("img").addEventListener("mouseout", function () {
     this.style.filter = "grayscale(100%)";
 });
+
+var a = document.querySelectorAll('.Sign');
+a.forEach(a => {
+    a.addEventListener('mouseover', () => {
+        a.style.backgroundColor = 'orange';
+    });
+
+    a.addEventListener('mouseout', () =>{
+        a.style.backgroundColor = 'lightseagreen';
+    })
+});
+
+
+var links = document.querySelectorAll('a');
+
+links.forEach(link => {
+    link.addEventListener('mouseover', () => {
+        link.style.color = 'lightseagreen';
+    });
+
+    link.addEventListener('mouseout', () => {
+        link.style.color = 'darkblue';
+    })
+});
+
+function showSidebar() {
+    var sidebar = document.getElementById('sidebar');
+    sidebar.style.display = (sidebar.style.display === 'none' || sidebar.style.display === '') ? 'block' : 'flex';
+}
+
+document.addEventListener('click', function (event) {
+    var sidebar = document.getElementById('sidebar');
+    var menuIcon = document.querySelector('.ri-menu-3-line');
+    if (!sidebar.contains(event.target) && event.target !== menuIcon) {
+        sidebar.style.display = 'none';
+    }
+});
+
+   
+
